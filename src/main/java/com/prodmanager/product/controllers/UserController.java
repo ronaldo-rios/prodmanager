6b485f087453e3b8336025187b4de92cacf004e4
Ronaldo <ronaldo-troupe@bitbucket.org>
2024-11-23 14:45:17 -0300

    feat:Implemented Environments Variables and test db connection

--- src/main/java/com/prodmanager/product/controllers/UserController.java
+++ src/main/java/com/prodmanager/product/controllers/UserController.java
@@ -1,19 +1,28 @@
 package com.prodmanager.product.controllers;
 
 import com.prodmanager.product.entities.User;
+import com.prodmanager.product.repositories.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Joana Maia", "jomaia@outlook.com", "55996508150", "123456");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
     }
 
 }
--- src/main/java/com/prodmanager/product/entities/User.java
+++ src/main/java/com/prodmanager/product/entities/User.java
@@ -1,12 +1,19 @@
 package com.prodmanager.product.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
+import jakarta.persistence.Entity;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
